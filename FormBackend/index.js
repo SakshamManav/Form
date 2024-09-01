e9fdae286ff3ea98a5405737caf880f1bd0c4afe
Saksham Manav <manavsaksham06@gmail.com>
2024-09-01 14:59:23 +0530

    Update index.js

--- FormBackend/index.js
+++ FormBackend/index.js
@@ -5,10 +5,10 @@ const connectToDatabase = require("./dbConnect");
 const port = process.env.PORT || 5000;
 connectToDatabase();
 app.use(cors({
-    origin: 'https://form-7w5g-sakshammanavs-projects.vercel.app/', // Allow requests from this origin
+    origin: 'https://form-7w5g-sakshammanavs-projects.vercel.app', // Allow requests from this origin
     methods: [ 'POST', ], // Allow these HTTP methods
     credentials:true,
-    // allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
   }));
 app.get("/", (req,res)=>{
     res.send("Hello");
